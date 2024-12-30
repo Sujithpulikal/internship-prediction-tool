1914887c67af18d12d8608e106f1af0afb1515d7
sujithpulikkal <sujithcp446@gmail.com>
2024-12-30 13:50:28 +0530

    html and css finished

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
-from flask import Flask,  request
+from flask import Flask,  request,render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "sample flask app"
+    return render_template('home.html')
+
+@app.route("/lead")
+def lead():
+    return render_template("lead.html")
+@app.route("/attrition")
+def attrition():
+    return render_template("attrition.html")
+@app.route("/placement")
+def placement():
+    return render_template("placement.html")
+@app.route("/about")
+def about():
+    return render_template("about.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
